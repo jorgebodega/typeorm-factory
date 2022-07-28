7e370c13314afc9ab049520ee7815f21fe1d43b5
Jorge Bodega Fernanz <jorge.bodega.f@gmail.com>
2022-07-28 21:26:16 +0200

    test: fix failing tests

--- test/fixtures/Pet.factory.ts
+++ test/fixtures/Pet.factory.ts
@@ -3,11 +3,13 @@ import { LazyInstanceAttribute } from '../../src'
 import { Factory } from '../../src/factory'
 import { Subfactory } from '../../src/subfactory'
 import type { FactorizedAttrs } from '../../src/types'
+import { dataSource } from './dataSource'
 import { Pet } from './Pet.entity'
 import { UserFactory } from './User.factory'
 
 export class PetFactory extends Factory<Pet> {
   protected entity = Pet
+  protected dataSource = dataSource
   protected attrs(): FactorizedAttrs<Pet> {
     return {
       name: faker.animal.insect(),
--- test/fixtures/User.factory.ts
+++ test/fixtures/User.factory.ts
@@ -2,9 +2,11 @@ import { faker } from '@faker-js/faker'
 import { Factory } from '../../src/factory'
 import type { FactorizedAttrs } from '../../src/types'
 import { User } from '../fixtures/User.entity'
+import { dataSource } from './dataSource'
 
 export class UserFactory extends Factory<User> {
   protected entity = User
+  protected dataSource = dataSource
 
   protected attrs(): FactorizedAttrs<User> {
     return {
