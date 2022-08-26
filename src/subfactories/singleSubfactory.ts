d67e6306f347add50f67d762ad985364dc375aec
Jorge Bodega <jorge.bodega.f@gmail.com>
2022-08-26 12:28:21 +0200

    fix: types should extends object

--- src/subfactories/singleSubfactory.ts
+++ src/subfactories/singleSubfactory.ts
@@ -2,7 +2,7 @@ import type { Factory } from '../factory'
 import type { Constructable, FactorizedAttrs } from '../types'
 import { BaseSubfactory } from './baseSubfactory'
 
-export class SingleSubfactory<T> extends BaseSubfactory<T> {
+export class SingleSubfactory<T extends object> extends BaseSubfactory<T> {
   constructor(factoryOrFactoryInstance: Constructable<Factory<T>> | Factory<T>, values?: Partial<FactorizedAttrs<T>>) {
     super(factoryOrFactoryInstance, values)
   }
